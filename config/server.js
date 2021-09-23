536a881ad5a6579092618ee3461acfccae69aa24
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-09-23 11:29:21 -0500

    add QuestionCategory 1-2

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'da82e5dc7ed2b86b555df4a58ec542db'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '137771ea-0125-4888-99f1-6718f3f43a62'
+};
